1081560d358c6fb0c0bd9b590e895ec205d54889
sutharrahul <sutharrahul119@gmail.com>
2024-12-27 15:27:42 +0530

    simmer effect error resolve

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -11,30 +11,27 @@ export default function Resume() {
     setIsLoading(false);
   };
 
-  const hendelClick = (e: MouseEvent<HTMLButtonElement>) => {
+  const downloadResumeClick = (e: MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
     window.open(
       "https://drive.google.com/file/d/1MrGOz9wfxH4qaN02FRLts6oEM1Vi3Jzg/view"
     );
   };
-
-  if (isLoading) {
-    <div
-      className="w-[80vw] sm:w-[80vw] md:w-[60vw] lg:w-[50vw] xl:w-[36vw] h-full flex flex-col justify-center gap-3 items-end rounded-2xl bg-white/5 p-2 isolate overflow-hidden relative 
-        before:absolute before:inset-0
-        before:-translate-x-full
-        before:animate-[shimmer_2s_infinite]
-        before:bg-gradient-to-r
-        before:from-transparent before:via-rose-100/10 before:to-transparent"
-    >
-      {" "}
-      <div className="h-[80vh] w-full rounded-lg bg-rose-100/10"></div>
-      <div className="font-medium py-1 px-6 w-28 h-9 rounded-lg bg-rose-100/10"></div>
-    </div>;
-  }
-
   return (
     <div className="flex flex-col items-end">
+      {isLoading && (
+        <div
+          className="w-[80vw] sm:w-[80vw] md:w-[60vw] lg:w-[50vw] xl:w-[36vw] h-full flex flex-col justify-center gap-3 items-end rounded-2xl bg-white/5 p-2 isolate overflow-hidden relative 
+          before:absolute before:inset-0
+          before:-translate-x-full
+          before:animate-[shimmer_2s_infinite]
+          before:bg-gradient-to-r
+          before:from-transparent before:via-rose-100/10 before:to-transparent"
+        >
+          <div className="h-[80vh] w-full rounded-lg bg-rose-100/10"></div>
+          <div className="font-medium py-1 px-6 w-28 h-9 rounded-lg bg-rose-100/10"></div>
+        </div>
+      )}
       <Image
         height={300}
         width={500}
@@ -44,7 +41,7 @@ export default function Resume() {
       />
       <button
         className="mt-5 font-medium py-1 px-6 bg-[#f0f0f0] text-black rounded-md"
-        onClick={hendelClick}
+        onClick={downloadResumeClick}
       >
         Download
       </button>
